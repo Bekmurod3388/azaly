4b28304760d1d72c1bd465f3f56ccb043cab1ff9
Lenovo <asadbek.polatov.02@mail.ru>
2022-06-15 15:48:30 +0500

    admin panel ozgartirildi

--- resources/views/admin/nav.blade.php
+++ resources/views/admin/nav.blade.php
@@ -1,5 +1,5 @@
 <nav
-    class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
+    class="layout-navbar navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
     id="layout-navbar"
 >
     <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
@@ -9,8 +9,64 @@
     </div>
 
     <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
-        <ul class="navbar-nav flex-row align-items-center ms-auto">
-            <li class="nav-item navbar-dropdown dropdown-user dropdown">
+        <ul class="navbar-nav flex-row align-items-center justify-content-around w-100">
+            <div class="d-flex justify-content-around align-items-center" style="width: 90%;">
+                @can('role-list')
+                    <li class="menu-item">
+                        <a href="{{ route('admin.users.index') }}" class="menu-link {{  request()->routeIs('admin.users.index') ? 'active' : 'text-black' }}">
+                            <i class="menu-icon tf-icons fas fa-users"></i>
+                            <div data-i18n="Analytics">Users Management</div>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="{{ route('admin.roles.index') }}" class="menu-link {{  request()->routeIs('admin.roles.index') ? 'active' : 'text-black' }}">
+                            <i class="menu-icon tf-icons fas fa-user-shield"></i>
+                            <div data-i18n="Analytics">Role Management</div>
+                        </a>
+                    </li>
+                @endcan
+                @can('category-list')
+                    <li class="menu-item">
+                        <a href="{{ route('admin.categories.index') }}" class="menu-link {{  request()->routeIs('admin.categories.index') ? 'active' : 'text-black' }}">
+                            <i class="menu-icon tf-icons fas fa-users"></i>
+                            <div data-i18n="Analytics">Categories</div>
+                        </a>
+                    </li>
+                @endcan
+                @can('category-list')
+                    <li class="menu-item">
+                        <a href="{{ route('admin.sizes.index') }}" class="menu-link {{  request()->routeIs('admin.sizes.index') ? 'active' : 'text-black' }}">
+                            <i class="menu-icon tf-icons fas fa-users"></i>
+                            <div data-i18n="Analytics">Sizes</div>
+                        </a>
+                    </li>
+                @endcan
+                @can('category-list')
+                    <li class="menu-item">
+                        <a href="{{ route('admin.warehouses.index') }}" class="menu-link {{  request()->routeIs('admin.warehouses.index') ? 'active' : 'text-black' }}">
+                            <i class="menu-icon tf-icons fas fa-users"></i>
+                            <div data-i18n="Analytics">WareHouse</div>
+                        </a>
+                    </li>
+                @endcan
+                @can('category-list')
+                    <li class="menu-item">
+                        <a href="{{ route('admin.shelf.index') }}" class="menu-link {{  request()->routeIs('admin.shelf.index') ? 'active' : 'text-black' }}">
+                            <i class="menu-icon tf-icons fas fa-users"></i>
+                            <div data-i18n="Analytics">Shelf</div>
+                        </a>
+                    </li>
+                @endcan
+                @can('category-list')
+                    <li class="menu-item">
+                        <a href="{{ route('admin.products.index') }}" class="menu-link {{  request()->routeIs('admin.products.index') ? 'active' : 'text-black' }}">
+                            <i class="menu-icon tf-icons fas fa-users"></i>
+                            <div data-i18n="Analytics">Products</div>
+                        </a>
+                    </li>
+                @endcan
+            </div>
+            <li class="nav-item navbar-dropdown dropdown-user dropdown" style="width: 10%">
                 <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                     <div class="avatar">
                         <img src="{{ asset('/assets/img/avatars/1.png') }}" alt class="w-px-40 h-auto rounded-circle"/>
