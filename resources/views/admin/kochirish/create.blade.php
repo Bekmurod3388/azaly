080c025d47faf57c95bf89ce01856cbedb69a81e
Fayzulla8387 <xojayevfayzulla7@gmail.com>
2022-08-06 04:49:46 +0500

    add button

--- resources/views/admin/kochirish/create.blade.php
+++ resources/views/admin/kochirish/create.blade.php
@@ -6,8 +6,12 @@
                 <div class="card-header">
                     <div class="row">
                         <div class="col-lg-12 margin-tb">
-                            <div class="pull-left">
+                            <div class="pull">
                                 <h2> Ko'chirish </h2>
+                            </div>
+
+                            <div class="pull-left">
+                                <a class="btn btn-primary" href="{{ route('admin.home') }}"> Orqaga </a>
                             </div>
                         </div>
                     </div>
@@ -20,15 +24,17 @@
 
                             <div class="col-xs-12 col-sm-12 col-md-12">
                                 <div class="form-group">
-                                    <strong> Omborxonani tanlang</strong>
+
                                     <select name="ombor1" required id="kochir"
                                             class="form-select form-control" required>
+                                        <option value="">Qayerdan  </option>
                                         @foreach($kochirish as $cat)
                                             <option value="{{$cat->id}}">{{$cat->name}}</option>
                                         @endforeach
                                     </select>
                                     <br>  <select name="ombor2" required id="kochir"
                                                   class="form-select form-control" required>
+                                        <option value="">Qayerga </option>
                                         @foreach($kochirish as $cat)
                                             <option value="{{$cat->id}}">{{$cat->name}}</option>
                                         @endforeach
