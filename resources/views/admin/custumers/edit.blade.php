8ed893c527ed3dd11078bd6f6ec85b2680fcb13d
Ruziev-Gofur <gafur_ru@bk.ru>
2022-08-12 17:04:37 +0500

    Keshbekni qo'shdim

--- resources/views/admin/custumers/edit.blade.php
+++ resources/views/admin/custumers/edit.blade.php
@@ -30,21 +30,6 @@
                             </div>
                         </div>
 
-                        <div class="col-xs-12 col-sm-12 col-md-12">
-                            <div class="form-group">
-                                <label for="services">Olgan Tavar</label>
-                                <input required="" type="text" name="received_goods" value="{{$custumers->received_goods}}" class="form-control" id="received_goods"
-                                       placeholder="Olgan Tavar">
-                            </div>
-                        </div>
-
-                        <div class="col-xs-12 col-sm-12 col-md-12">
-                            <div class="form-group">
-                                <label for="services">Bonus</label>
-                                <input required="" type="number" name="bonus_money" class="form-control" id="bonus_money"
-                                       placeholder="Bonus" value="{{$custumers->bonus_money}}">
-                            </div>
-                        </div>
 
                         <div class="col-xs-12 col-sm-12 col-md-12">
                             <div class="form-group">
@@ -58,31 +43,24 @@
                             <div class="form-group">
                                 <label for="services">Passport</label>
                                 <input required="" type="text" name="passport" class="form-control" id="passport"
-                                       placeholder="AA 0000000"value="{{$custumers->passport}}">
+                                       placeholder="AA0000000"value="{{$custumers->passport}}">
                             </div>
                         </div>
 
-                        <div class="col-xs-12 col-sm-12 col-md-12">
-                            <div class="form-group">
-                                <label for="services">Chegirma</label>
-                                <input required="" type="number" name="discount" class="form-control" id="discount"
-                                       placeholder="Chegirma" value="{{$custumers->discount}}">
-                            </div>
-                        </div>
+
 
                         <div class="col-xs-12 col-sm-12 col-md-12">
                             <div class="form-group">
                                 <label for="services">Keshbek</label>
-                                <input required="" type="number" name="keashback" class="form-control" id="discount"
-                                       placeholder="Keshbek" value="{{$custumers->keshback}}">
+                                <input required="" type="number" name="keashback" class="form-control" id="keashback"
+                                       placeholder="Keshback" value="{{$custumers->keshback}}">
                             </div>
                         </div>
                         <div class="col-xs-12 col-sm-12 col-md-12">
                             <div class="form-group">
-                                <label for="building"> Omborxona </label>
-                                <select name="categorea" required id="warehouse" class="form-select form-control"
-                                        required>
-                                    <option value=""> Omborxona tanlang</option>
+                                <label for="building"> Kategoriya </label>
+                                <select name="categorea" required id="warehouse" class="form-select form-control" required>
+                                    <option value=""> Kategoriya tanlang</option>
                                     @foreach( $cost as $cat)
                                         <option value="{{$cat->id}}">{{$cat->name}}</option>
                                     @endforeach
